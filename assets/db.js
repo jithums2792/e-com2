69ed9e128fa936422bcc329f52e8c55853a8f739
jithums2792 <greenox2792@gmail.com>
2020-05-01 20:29:26 +0530

    new updates

--- assets/db.js
+++ assets/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const _URL = 'mongodb://127.0.0.1:27017/ecom'
 const _URI = 'mongodb+srv://jithu2792:jithu@123@testdb-scppv.mongodb.net/test?retryWrites=true&w=majority'
-mongoose.connect(_URL, {useNewUrlParser: true,useUnifiedTopology: true},(err)=>{
+mongoose.connect(_URI, {useNewUrlParser: true,useUnifiedTopology: true},(err)=>{
     console.log(err);
 })
